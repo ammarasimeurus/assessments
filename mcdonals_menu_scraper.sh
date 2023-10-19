71292ceaeeace8c4df6e64f7cbf2b5330f40039b
ammarasimeurus <148402457+ammarasimeurus@users.noreply.github.com>
2023-10-19 22:39:04 +0500

    Add files via upload

---
+++ mcdonals_menu_scraper.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+rm -rf menu
+echo "scraping the Mcdonals site ..."
+raw=`curl -s https://mcdonalds.com.pk/full-menu/ | grep -Po "category-title.*?</span>"`
+echo $raw
+
+
+echo "processing the raw input ..."
+#removing useless values in result
+result=$(echo "$raw" | sed "s/category-title\">//g")
+result=$(echo "$result" | sed "s/<\/span>//g")
+result=$(echo "$result" | sed "s/amp;//g")
+
+
+#processing the names 
+filenames=$(echo "$result" | tr ' ' '-')
+filenames=$(echo "$filenames" | tr '\n' ' ')
+filenames="${filenames#* }"
+filenames=$(echo "$filenames" | sed "s/&/and/g")
+
+
+
+
+#preparing string for futher scraping
+
+lowercase_string=$(echo "$filenames" | tr '[:upper:]' '[:lower:]')
+filenames=$(echo "$lowercase_string" | tr 'é' 'e')
+echo $lowercase_string
+
+echo "creating files ..."
+#creating files in a directory
+mkdir menu
+cd menu
+touch $filenames
+
+
+
+
+echo -e "\nThe Menu Categories are "
+
+for file in $filenames;do
+
+echo -e "$file"
+
+done
+
+
+#curling each url of the menu
+for file in $filenames;do
+
+echo "scraping the $file menu ..."
+raw=`curl -s https://mcdonalds.com.pk/full-menu/$file/ | grep -Po "categories-item-name.*?</span>"`
+#echo $raw
+
+echo "processing the raw input ..."
+#removing useless values in result
+result=$(echo "$raw" | sed "s/categories-item-name\">//g")
+result=$(echo "$result" | sed "s/<\/span>/,/g")
+result=$(echo "$result" | sed "s/amp;//g")
+
+
+echo $result > $file
+#processing the names 
+
+done
+
